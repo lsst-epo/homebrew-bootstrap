7b2fc023ed6f5b139e2a4130f3edae05ccd41608
Zach Davis <zach@castironcoding.com>
2020-04-23 14:40:30 -0700

    [C] Remove comment

--- Formula/phpenv.rb
+++ Formula/phpenv.rb
@@ -1,9 +1,6 @@
 class Phpenv < Formula
   desc "PHP version manager"
-  homepage "https://github.com/castiron/phpenv#readme"
-#  url "https://github.com/castiron/phpenv/archive/v1.0.0.tar.gz"
-#  sha256 "baea56d63e5fa60ea5658360dc05665ff770184ce826191ce268ca73feaf3e80"
-#  head "https://github.com/castiron/phpenv.git"
+  homepage "https://github.com/phpenv/phpenv#readme"
   url "https://github.com/phpenv/phpenv", :using => :git, :revision => "9b7e4e1c0083c46be69f4c6d063f78c18654aad1"
   head "https://github.com/phpenv/phpenv.git"
   version "1.1"
